bb93209c6a29853c8485c901c627ab034201f396
David Stein <dstein131@gmail.com>
2023-06-26 17:48:35 -0400

    co

--- src/Company.js
+++ src/Company.js
@@ -23,6 +23,39 @@ function Company() {
             }}
             >We Are SRI</h1>
         </div>
+
+        <div
+        style={{
+            width: "100%",
+            minHeight: "25rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingLeft: "7rem",
+            paddingRight: "7rem",
+        }}>
+            <div
+            style={{
+                width: "40%",
+                color: "#F26B3F",
+                wordBreak: "break-word",
+            }}>
+                <h3>Serving governments and investors for more than 30 years.</h3>
+            </div>
+            <div 
+            style={{
+                width: "60%"
+            }}>
+            <p>
+            At SRI, we are driven to create simple solutions for elected officials and investors. We work to relieve the tension that can come from tax sales or mortgage foreclosures. Using customizable technology, we are able to serve every corner of the country, and today, work with more than 300 local government offices
+            </p>
+            </div>
+        </div>
+
+                
+
+
+
         </div>
     );
     }
