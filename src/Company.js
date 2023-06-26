3340a610fa5f9e04805a72844c7097631e5e4111
David Stein <dstein131@gmail.com>
2023-06-26 17:39:31 -0400

    co

--- src/Company.js
+++ src/Company.js
@@ -1,9 +1,28 @@
 import React from "react";
+import image1 from "../src/images/sri-how-we-work-header.png";
 
 function Company() {
     return (
         <div>
-        <h1>Company</h1>
+           
+      <div
+        style={{
+          width: "100%",
+          height: "25rem",
+          backgroundImage: `url(${image1})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "right top",
+          display: "flex",
+          alignItems: "center",
+        }}>
+            <h1
+            style={{
+                color: "White",
+                marginLeft: "10rem",
+            }}
+            >We Are SRI</h1>
+        </div>
         </div>
     );
     }
